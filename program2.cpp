6eb45c7c199e38a6d2c01051c3427e876fdb2a58
Atish Shanbhag <atishshanbhag03.com>
2023-12-25 14:08:58 +0530

    ft_secondProgram

---
+++ program2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = 5;
+    std::cout<<"Sum of two numbers:"<<a+b<<"\n";
+    std::cout<<"Sub of two numbers:"<<a-b<<"\n";
+    std::cout<<"Multiplication of two numbers:"<<a*b;
+
+}
