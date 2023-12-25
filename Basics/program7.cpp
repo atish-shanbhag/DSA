8666155a16e2e69e6e2fd85b4ef24d48c4e342c2
Atish Shanbhag <atishshanbhag03.com>
2023-12-25 16:28:36 +0530

    cpp

---
+++ Basics/program7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    cout<<(int)ch;
+    int x;
+    cin>>x;
+    char ch1 = (char)x;
+    cout<<ch1;
+} // ascii both ways
