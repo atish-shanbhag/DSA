6111f88203c3729499866600874efe311a397423
Atish Shanbhag <atishshanbhag03.com>
2023-12-25 15:49:10 +0530

    programs

---
+++ program5.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int x; int y; int z;
+    cout<<"Kindly provide three number"<<endl;
+    cin>>x>>y>>z;
+    cout<<"Product of three numbers: "<<x*y*z;
+}
