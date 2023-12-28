5b746994bf4afabc41e6227413af05e87d4db516
Atish Shanbhag <atishshanbhag03.com>
2023-12-28 23:21:40 +0530

    patterns

---
+++ Basics/program55.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m,n;
+    cin>>m>>n;
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if (i == 1 || i == m || j == 1 || j == n)  cout<<"* ";
+            else cout <<"  ";
+        }
+        cout<<endl;
+    }
+}
+
+// 4 5
+// * * * * *
+// *       *
+// *       *
+// * * * * *
