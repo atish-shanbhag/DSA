9e3ef72994bafeff6a41770c8478e12abc8d2e1f
Atish Shanbhag <atishshanbhag03.com>
2024-01-08 23:12:27 +0530

    leetcode

---
+++ leetcode/PascalTraingleArray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        for(int i=0;i<numRows;i++){
+            vector<int> row(i+1, 1);
+            for(int j=1;j<i;j++){
+                row[j] = result[i-1][j-1] + result[i-1][j];
+            }
+            result.push_back(row);
+        }
+        return result;
+    }
+};
+
+// Input: numRows = 5
+// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
