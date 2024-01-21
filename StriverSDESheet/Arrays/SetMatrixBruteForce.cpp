adf743b652fe41e33e63d0a9b80d29300a387da2
Atish Shanbhag <atishshanbhag03.com>
2024-01-22 05:09:27 +0530

    arrays

---
+++ StriverSDESheet/Arrays/SetMatrixBruteForce.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void markRow(vector<vector<int>>& matrix, int n, int m, int i){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j]!=0){
+                matrix[i][j] = -1;
+            }
+        }
+    }
+
+     void markCol(vector<vector<int>>& matrix, int n, int m, int j){
+        for(int i=0;i<n;i++){
+            if(matrix[i][j]!=0){
+                matrix[i][j] = -1;
+            }
+        }
+    }
+
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j] == 0){
+                    markRow(matrix, n, m, i);
+                    markCol(matrix, n, m, j);
+                }
+            }
+        }
+
+           for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j] == -1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
